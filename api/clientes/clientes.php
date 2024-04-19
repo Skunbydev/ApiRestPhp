147822377a0cc6490c91f33a9a374f0006c2582c
skunbydev <skunbydev@gmail.com>
2024-04-19 14:41:04 -0300

    ajusting logica

--- api/clientes/clientes.php
+++ api/clientes/clientes.php
@@ -1,20 +1,18 @@
 <?php
 
 if ($api == 'clientes') {
+
   if ($method == "GET") {
     include_once ("get.php");
-  }
-  if ($method == "POST") {
+  } elseif ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "PUT") {
+    include_once ("put.php");
+  } elseif ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "DELETE") {
+    include_once ("delete.php");
+  } elseif ($method == "POST") {
     include_once ("post.php");
   }
-  if ($method == "POST" && $_POST['_method'] == "PUT") {
-    include_once ("put.php");
-  }
-  if ($method == "POST" && $_POST['_method'] == "DELETE") {
-    include_once ("delete.php");
-  }
-
 }
+
 
 
 ?>
